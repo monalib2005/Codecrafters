034d7359ba3579fc35d9d110a2664bc6cc81812e
Monali Bhoyar <monalib909@gmail.com>
2024-07-22 18:13:35 +0530

    Inital commit

--- script.js
+++ script.js
@@ -12,22 +12,118 @@ gsap.from("#btn",{opacity:0,duration:1,scale:2});
 WebGLShaderPrecisionFormat
 gsap.from("#right h1", {
     fontSize:64,
-    duration: 1,
+    duration: 1.5,
     yoyo: true,
-    ease: "bounce.out"
+    ease: "bounce.out",
+    scrollTrigger:{
+        trigger:"#right h1",
+        start:"top 50%",
+        scrub:true,
+        scroller:"body"
+    },
 });
 gsap.from("#left h1", {
     //y: -100,
     fontSize:64,
-    duration: 0.7,
+    duration: 1.5,
     yoyo: true,
-    ease: "bounce.out"
+    ease: "bounce.out",
+    
+
 });
-gsap.from("#right h3 ", { x: -400, duration: 1.2 });
-gsap.from("#left h3 ", { x: 1400, duration: 1.2 });
+gsap.from("#right h3 ", { x: -400, duration: 1.5 ,scrollTrigger:{
+    trigger:"#right h1",
+    start:"top 50%",
+    scrub:true,
+    scroller:"body"
+},}
+);
+gsap.from("#left h3 ", { x: 1400, duration: 1.5,  scrollTrigger:{
+    trigger:"#right h1",
+    start:"top 50%",
+    scrub:true,
+    scroller:"body"
+},});
 
 
 
-image.addEventListener('mouseout', () => {
-  gsap.to(image, { scale: 1, duration: 0.3 });
+// image.addEventListener('mouseout', () => {
+//   gsap.to(image, { scale: 1, duration: 0.3 });
+// });
+//3
+gsap.to("#img1,#img3,#img5",{
+    y:-50,
+    duration:2,
+    repeat: -1, 
+    yoyo: true,
+    ease: "power1.inOut"
 });
+gsap.to("#img2,#img4,#img6",{
+    y:50,
+    duration:2,
+    repeat: -1, 
+    yoyo: true,
+    ease: "power1.inOut"
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const icecreams = document.querySelectorAll("#milkimg");
+  
+    function floatIceCreams() {
+        icecreams.forEach(icecream => {
+            gsap.to(icecream, {
+                y: -20,
+                duration: 1,
+                yoyo: true,
+                repeat: -1,
+                ease: "power1.inOut"
+            });
+        });
+    }
+  
+    floatIceCreams();
+  });
+  gsap.from("#para",{
+    opacity:0,
+    duration:2
+  })
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    
+    const icecreams = document.querySelectorAll(".icecreamImage");
+  
+   
+    function floatIceCreams() {
+        icecreams.forEach(icecream => {
+            gsap.to(icecream, {
+                y: -20,
+                duration: 1,
+                yoyo: true,
+                repeat: -1,
+                ease: "power1.inOut"
+            });
+        });
+    }
+  
+   
+    floatIceCreams();
+  });
+
+  const timelineEvents = document.querySelectorAll('.timeline-event');
+
+  function handleScroll() {
+    timelineEvents.forEach((event, index) => {
+      const rect = event.getBoundingClientRect();
+      const triggerPoint = window.innerHeight * 0.8; // Adjust trigger point as needed
+  
+      if (rect.top < triggerPoint) {
+        event.classList.add('fadeInUp');
+        event.classList.remove('fadeOutDown');
+      } else {
+        event.classList.remove('fadeInUp');
+        event.classList.add('fadeOutDown');
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', handleScroll);
--- script1.js
+++ script1.js
@@ -14,237 +14,136 @@ gsap.from("#image2",{x:-500,duration:1,opacity:0,delay:0.5})
 gsap.from("#image3",{x:-500,duration:1,opacity:0,delay:1})
 gsap.from("#image4",{x:-500,duration:1,opacity:0,delay:1.5})
 gsap.from("#image5",{x:-500,duration:1,opacity:0,delay:2})
-const hideButton = document.getElementById('image1');
-const initialContent = document.getElementById('hr');
 
-hideButton.addEventListener('click', () => {
-  initialContent.style.display = 'none';
+const hideButton1 = document.getElementById('image1');
+const initialContent1 = document.getElementById('hr');
+
+hideButton1.addEventListener('click', () => {
+  initialContent1.style.display = 'none';
 });
-const imageContainer1 = document.querySelector('.pint1');
-const image1 = imageContainer1.querySelector('img'); // Corrected variable name
-const imageArray1 = ['pint12.avif', 'pint13.webp', 'pint11.webp']; // Replace with your image names
+const hideButton2 = document.getElementById('image2');
+const initialContent2 = document.getElementById('hr');
 
-let currentIndex1 = 0;
+hideButton2.addEventListener('click', () => {
+  initialContent2.style.display = 'none';
+});
+const hideButton3 = document.getElementById('image3');
+const initialContent3 = document.getElementById('hr');
 
-function changeImage1() {
-  currentIndex1 = (currentIndex1 + 1) % imageArray1.length; // Corrected variable name
-  image1.src = imageArray1[currentIndex1]; // Corrected variable name
+hideButton3.addEventListener('click', () => {
+  initialContent3.style.display = 'none';
+});
+const hideButton4 = document.getElementById('image4');
+const initialContent4 = document.getElementById('hr');
+
+hideButton4.addEventListener('click', () => {
+  initialContent4.style.display = 'none';
+});
+const hideButton5 = document.getElementById('image5');
+const initialContent5 = document.getElementById('hr');
+
+hideButton5.addEventListener('click', () => {
+  initialContent5.style.display = 'none';
+});
+const hideButton6 = document.getElementById('image1');
+const initialContent6 = document.getElementById('main');
+
+hideButton6.addEventListener('click', () => {
+  initialContent6.style.backgroundColor = '#CDB4DB';
+});
+const hideButton7 = document.getElementById('image2');
+const initialContent7 = document.getElementById('main');
+
+hideButton7.addEventListener('click', () => {
+  initialContent7.style.backgroundColor = '#CDB4DB';
+});
+const hideButton8 = document.getElementById('image3');
+const initialContent8 = document.getElementById('main');
+
+hideButton8.addEventListener('click', () => {
+  initialContent8.style.backgroundColor = '#CDB4DB';
+});
+const hideButton9 = document.getElementById('image4');
+const initialContent9 = document.getElementById('main');
+
+hideButton9.addEventListener('click', () => {
+initialContent9.style.backgroundColor = '#CDB4DB';
+});
+const hideButton10 = document.getElementById('image5');
+const initialContent10 = document.getElementById('main');
+
+hideButton10.addEventListener('click', () => {
+  initialContent10.style.backgroundColor = '#CDB4DB';
+});
+
+const imageConfigs = [
+  { selector: '.pint1', images: ['pint12.avif', 'pint13.webp', 'pint11.webp'] },
+  { selector: '.pint2', images: ['pint21.webp', 'pint22.webp', 'pint24.avif'] },
+  { selector: '.pint3', images: ['pint31.jpg', 'pint32.jpg', 'pint33.jpg'] },
+  { selector: '.pint4', images: ['pint41.avif', 'pint42.avif', 'pint43.avif'] },
+  { selector: '.dough1', images: ['dough1.webp', 'dough11.webp', 'dough12.webp'] },
+  { selector: '.dough2', images: ['dough21.webp', 'dough22.jpg', 'dough2.webp'] },
+  { selector: '.dough3', images: ['dough31.jpg', 'dough32.webp', 'dough3.webp'] },
+  { selector: '.dough4', images: ['dough4.jpeg', 'dough41.jpeg','dough4.jpeg'] },
+  { selector: '.dairy1', images: ['dairy1.webp', 'dairy2.webp','dairy3.webp'] },
+  { selector: '.dairy2', images: ['dairy21.avif', 'dairy22.jpg','dairy23.avif'] },
+  { selector: '.dairy3', images: ['dairy31.avif', 'dairy32.avif','dairy33.avif'] },
+  { selector: '.dairy4', images: ['dairy4.avif', 'dairy41.avif','dairy42.avif'] },
+  { selector: '.core1', images: ['core11.avif', 'core12.avif','core13.avif'] },
+  { selector: '.core2', images: ['core21.avif', 'core22.avif','core23.jpg'] },
+  { selector: '.core3', images: ['core3.avif', 'core31.avif','core32.webp'] },
+  { selector: '.core4', images: ['core4.avif', 'core41.avif','core42.avif'] },
+  { selector: '.topped1', images: ['topped1.avif', 'topped11.avif','topped12.avif'] },
+  { selector: '.topped2', images: ['topped2.webp', 'topped21.webp','topped23.webp'] },
+  { selector: '.topped3', images: ['topped3.avif', 'topped31.webp','topped32.avif'] },
+  { selector: '.topped4', images: ['topped4.avif', 'topped41.avif','topped42.avif'] }
+];
+
+// Function to change the image
+function changeImage(imageContainer, images) {
+  let currentIndex = 0;
+  const imageElement = imageContainer.querySelector('img');
+  
+  return function() {
+      currentIndex = (currentIndex + 1) % images.length;
+      imageElement.src = images[currentIndex];
+  };
 }
 
-setInterval(changeImage1, 2000); // Change image every 3 seconds (3000 ms)
+// Iterate over each image configuration and set the interval
+imageConfigs.forEach(config => {
+  const imageContainer = document.querySelector(config.selector);
+  if (imageContainer) {
+      setInterval(changeImage(imageContainer, config.images), 2000);
+  }
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const cow = document.getElementById("cowImage");
+  const icecreams = document.querySelectorAll(".icecreamImage");
 
-const imageContainer2 = document.querySelector('.pint2');
-const image2 = imageContainer2.querySelector('img'); // Corrected variable name
-const imageArray2 = ['pint21.webp', 'pint22.webp', 'pint24.avif']; // Replace with your image names
+  function moveCow() {
+      gsap.to(cow, {
+          //x: window.innerWidth - cow.width,
+          x:900,
+          duration: 8,
+          yoyo: true,
+          repeat: -1,
+          ease: "power1.inOut"
+      });
+  }
 
-let currentIndex2 = 0;
+  function floatIceCreams() {
+      icecreams.forEach(icecream => {
+          gsap.to(icecream, {
+              y: -20,
+              duration: 1,
+              yoyo: true,
+              repeat: -1,
+              ease: "power1.inOut"
+          });
+      });
+  }
 
-function changeImage2() {
-  currentIndex2 = (currentIndex2 + 1) % imageArray2.length; // Corrected variable name
-  image2.src = imageArray2[currentIndex2]; // Corrected variable name
-}
-
-setInterval(changeImage2, 2000); // Change image every 3 seconds (3000 ms)
-const imageContainer3 = document.querySelector('.pint3');
-const image3 = imageContainer3.querySelector('img'); // Corrected variable name
-const imageArray3 = ['pint31.jpg', 'pint32.jpg', 'pint33.jpg']; // Replace with your image names
-
-let currentIndex3 = 0;
-
-function changeImage3() {
-  currentIndex3 = (currentIndex3 + 1) % imageArray3.length; // Corrected variable name
-  image3.src = imageArray3[currentIndex3]; // Corrected variable name
-}
-setInterval(changeImage3,2000);
-const imageContainer5 = document.querySelector('.dough1');
-const image5 = imageContainer4.querySelector('img'); // Corrected variable name
-const imageArray5 = ['dough1.webp', 'dough11.webp', 'dough12.webp']; // Replace with your image names
-
-let currentIndex5 = 0;
-
-function changeImage5() {
-  currentIndex5 = (currentIndex5 + 1) % imageArray5.length; // Corrected variable name
-  image5.src = imageArray5[currentIndex5]; // Corrected variable name
-}
-
-setInterval(changeImage5, 2000); // Change image every 3 seconds (3000 ms)
-const imageContainer6 = document.querySelector('.dough2');
-const image6 = imageContainer6.querySelector('img'); // Corrected variable name
-const imageArray6 = ['dough2.webp', 'dough21.webp', 'dough22.webp']; // Replace with your image names
-
-let currentIndex6 = 0;
-
-function changeImage6() {
-  currentIndex6 = (currentIndex6 + 1) % imageArray6.length; // Corrected variable name
-  image6.src = imageArray6[currentIndex6]; // Corrected variable name
-}
-
-setInterval(changeImage6, 2000); // Change image every 3 seconds (3000 ms)
-const imageContainer7 = document.querySelector('.dough3');
-const image7 = imageContainer7.querySelector('img'); // Corrected variable name
-const imageArray7 = ['dough3.webp', 'dough31.jpg', 'dough32.webp']; // Replace with your image names
-
-let currentIndex7 = 0;
-
-function changeImage7() {
-  currentIndex7 = (currentIndex7 + 1) % imageArray7.length; // Corrected variable name
-  image7.src = imageArray7[currentIndex7]; // Corrected variable name
-}
-
-setInterval(changeImage7, 2000); // Change image every 3 seconds (3000 ms)
-const imageContainer8 = document.querySelector('.dough4');
-const image8 = imageContainer8.querySelector('img'); // Corrected variable name
-const imageArray8 = ['dough4.jpeg', 'dough41.jpeg', 'dough4.jpeg']; // Replace with your image names
-
-let currentIndex8 = 0;
-
-function changeImage8() {
-  currentIndex8 = (currentIndex8 + 1) % imageArray8.length; // Corrected variable name
-  image8.src = imageArray8[currentIndex8]; // Corrected variable name
-}
-
-setInterval(changeImage8, 2000); // Change image every 3 seconds (3000 ms)
-const imageContainer9 = document.querySelector('.dairy1');
-const image9 = imageContainer9.querySelector('img'); // Corrected variable name
-const imageArray9 = ['dairy1.webp', 'dairy2.webp', 'dairy3.webp']; // Replace with your image names
-
-let currentIndex9 = 0;
-
-function changeImage9() {
-  currentIndex9 = (currentIndex9 + 1) % imageArray9.length; // Corrected variable name
-  image9.src = imageArray9[currentIndex9]; // Corrected variable name
-}
-
-setInterval(changeImage9, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
-// const imageContainer4 = document.querySelector('.pint4');
-// const image4 = imageContainer4.querySelector('img'); // Corrected variable name
-// const imageArray4 = ['pint41.avif', 'pint42.avif', 'pint43.avif']; // Replace with your image names
-
-// let currentIndex4 = 0;
-
-// function changeImage4() {
-//   currentIndex4 = (currentIndex4 + 1) % imageArray3.length; // Corrected variable name
-//   image4.src = imageArray4[currentIndex4]; // Corrected variable name
-// }
-
-// setInterval(changeImage4, 2000); // Change image every 3 seconds (3000 ms)
+  moveCow();
+  floatIceCreams();
+});
